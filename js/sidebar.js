8e7ed0b66e86be858e65959764d4fc7ce322447b
Andy Kamath <andy@hackgician.net>
2015-04-23 19:40:44 -0400

    Added important files

---
+++ js/sidebar.js
@@ -0,0 +1,9 @@
+$(".navitem").click(function(e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+});
+
+$("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+});
