165b35e8335d1efc28383386dd169750b960af5e
Andy Kamath <andy@andykamath.net>
2015-05-06 19:20:31 -0400

    Update action_answer_placelist.php

--- action_answer_placelist.php
+++ action_answer_placelist.php
@@ -1,3 +1,7 @@
 <?php
-	echo file_get_contents("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=".$_GET['action']."&radius=5000&types=food&key=AIzaSyAZwbQCDTf-AKn0dvdNahrgXOqkZQTydTQ");
+	$json = file_get_contents("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=".$_GET['action']."&radius=5000&types=food&key=AIzaSyAZwbQCDTf-AKn0dvdNahrgXOqkZQTydTQ");
+	$data = json_decode($json, true);
+	foreach($data as $item) { //foreach element in $arr
+	    echo $item['results']['name']; //etc
+	}
 ?>
