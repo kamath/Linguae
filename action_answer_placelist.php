b628c7b5bc747dfac340e0a8651733ae8f658268
Andy Kamath <andy@hackgician.net>
2015-05-09 02:01:05 -0400

    wanna fight, heroku

--- action_answer_placelist.php
+++ action_answer_placelist.php
@@ -4,8 +4,6 @@
 	foreach ($jsonContent['results'] as $result) {
     	// now you have the $result array that contains the location of the place
     	// and the name ($result['formatted_address'], $result['name']) and other data.
-    	echo "<img src=\"".$result['icon']."\"><br>";
-    	echo $result['name']."<br>";
-    	echo $result['vicinity']."<br>";
+    	echo $result['name'].",";
 	}
 ?>
