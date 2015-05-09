a10a104352e4d139dce0ae6330058d00641c73ef
Andy Kamath <andy@hackgician.net>
2015-05-09 02:16:58 -0400

    SWAG

--- action_answer_placelist.php
+++ action_answer_placelist.php
@@ -4,6 +4,6 @@
 	foreach ($jsonContent['results'] as $result) {
     	// now you have the $result array that contains the location of the place
     	// and the name ($result['formatted_address'], $result['name']) and other data.
-    	echo $result['name'].",";
+    	echo $result['name'].":".$result['vicinity'].";";
 	}
 ?>
