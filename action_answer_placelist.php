807561f9d0671704e8f36abdc9123bfc6a4b2d56
Andy Kamath <andy@hackgician.net>
2015-05-09 19:35:08 -0400

    swag

--- action_answer_placelist.php
+++ action_answer_placelist.php
@@ -13,7 +13,7 @@
                     $location = $result['geometry']['location']['lat'].",".$result['geometry']['location']['lng'];
                     $spacename = str_replace(" ", "+", $result['name']);
                     $spaceaddress = str_replace(" ", "+", $result['vicinity']);
-                    $loc = "http://www.google.com/maps/dir/35.1093724,-80.748323/".$spacename.",+".$spaceaddress."/@".$location.",12z";
+                    $loc = "http://www.google.com/maps/dir/".$_GET['action']."/".$spacename.",+".$spaceaddress."/@".$location.",12z";
     			    echo "<img src=\"https://maps.googleapis.com/maps/api/place/photo?key=AIzaSyDsBsq9Dei-OnXAYzrSZHC2s6Mvx8_XCxo&photoreference=".$r['photo_reference']."&maxheight=200\"><br>".$result['name']."`".$result['vicinity']."ccc".$loc.";";	
                 }
     		}
